d55a1de03f4c6d3d61305bc72bb4dbcd8a9ae8f2
susolr <jlopezpeque@gmail.com>
2022-03-15 18:48:18 +0100

    P2

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
 function mostrarComentarios(){
-    document.getElementById("comentarios-container").style.display=flex;
+    document.getElementById("comentarios-sidepanel").style.width= "40%";
 }
 
 function esconderComentarios(){
+    document.getElementById("comentarios-sidepanel").style.width= "0";
+}
 
+function mostrarComentarioOculto(){
+    document.getElementById("comentario-oculto").style.display= "block";
 }
 
 function comprobarEmail(){
