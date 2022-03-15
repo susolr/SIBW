7334ebacfd4582204654d3aa6a9885e863790b38
susolr <jlopezpeque@gmail.com>
2022-03-15 17:43:28 +0100

    Creado script

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function mostrarComentarios(){
+    document.getElementById("comentarios-container").style.display=flex;
+}
+
+function esconderComentarios(){
+
+}
+
+function comprobarEmail(){
+   
+}
+
+function recibirFormulario(){
+
+}
