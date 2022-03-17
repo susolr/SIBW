76f1cf6206f6799d078452667f396535f817196b
susolr <jlopezpeque@gmail.com>
2022-03-17 13:14:22 +0100

    Eliminado zip

--- scripts.js
+++ scripts.js
@@ -10,10 +10,27 @@ function mostrarComentarioOculto(){
     document.getElementById("comentario-oculto").style.display= "block";
 }
 
-function comprobarEmail(){
-   
+function comprobarEmail(email){ 
+    var emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+
+    var correo_correcto = emailRegex.test(email.value);
+
+    return correo_correcto;
 }
 
-function recibirFormulario(){
+function comprobarNombre(){
+
+}
+
+function mostrarError(){
+
+}
+
+function publicarComentario(){
+    var email = document.getElementById('email').innerHTML();
+    var email_correcto = comprobarEmail(email);
+    if(email_correcto){
+        
+    }
 
 }
