af1ffebf414c92b0c74084f9ed1f4dccdd50a4a0
susolr <jlopezpeque@gmail.com>
2022-03-18 01:04:38 +0100

    Comprobar palabras

--- scripts.js
+++ scripts.js
@@ -3,12 +3,14 @@ var comentarios = new Array();
 var comentario1 = {
     autor:"Jesús",
     fecha: "17/03/2021",
+    hora: "10:33",
     texto: "Producto altamente recomendable debido a su gran calidad"
 };
 
 var comentario2 = {
     autor:"Fran",
     fecha: "13/02/2021",
+    hora: "18:53",
     texto: "Muy mala experiencia con el producto"
 };
 
@@ -30,34 +32,61 @@ function mostrarComentarios(){
         para.id = id;
 
         /*Autor*/
+        //Label
+        var p = document.createElement("p");
+        p.className ="label";
         var label = document.createTextNode(authLabel);
         label.className = "autor-label";
         label.id = "autor-label" + ind;
-        para.appendChild(label);
+        p.appendChild(label);
+        para.appendChild(p);
+        
+        //Contenido
+        p = document.createElement("p");
+        p.className = "contenido";
         var content = document.createTextNode(comentarios[i].autor);
         content.className = "autor";
         content.id = "autor" + ind;
-        para.appendChild(content);
+        p.appendChild(content);
+        para.appendChild(p);
 
         /*Fecha y hora*/
+        //Label
+        p = document.createElement("p");
+        p.className ="label";
         label = document.createTextNode(dateLabel);
         label.className = "date-label";
         label.id = "date-label" + ind;
-        para.appendChild(label);
-        content = document.createTextNode(comentarios[i].fecha);
+        p.appendChild(label);
+        para.appendChild(p);
+
+        //Contenido
+        p = document.createElement("p");
+        p.className = "contenido";
+        content = document.createTextNode(comentarios[i].fecha + " " + comentarios[i].hora);
         content.className = "date";
         content.id = "date" + ind;
-        para.appendChild(content);
+        p.appendChild(content);
+        para.appendChild(p);
 
-        /*Contenido*/
+        /*Comentario*/
+        //Label
+        p = document.createElement("p");
+        p.className ="label";
         label = document.createTextNode(contentLabel);
         label.className = "content-label";
         label.id = "content-label" + ind;
-        para.appendChild(label);
+        p.appendChild(label);
+        para.appendChild(p);
+
+        //Contenido
+        p = document.createElement("p");
+        p.className = "contenido";
         content = document.createTextNode(comentarios[i].texto);
         content.className = "content";
         content.id = "content" + ind;
-        para.appendChild(content);
+        p.appendChild(content);
+        para.appendChild(p);
 
         const element = document.getElementById("comentarios-sidepanel");
         const child = document.getElementById("formulario-comentario");
@@ -96,30 +125,66 @@ function mostrarComentarioOculto(){
 function comprobarEmail(email){ 
     var emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
 
-    var correo_correcto = emailRegex.test(email.value);
+    var correo_correcto = emailRegex.test(email);
 
     return correo_correcto;
 }
 
-function comprobarNombre(){
-
+function comprobarNombre(nombre){
+    return true;
 }
 
-function mostrarError(){
+function comprobarContenido(contenido){
+    return true;
+}
 
+function mostrarError(mensaje){
+    alert(mensaje);
 }
 
 function publicarComentario(){
-    var email = document.getElementById('email').innerText;
-    console.log(email);
-    //var email_correcto = comprobarEmail(email);
-    //if(!email_correcto){
-    //    return;
-    //}
-    var nombre = document.getElementById('nombre').innerText;
-    console.log(nombre);
+    var email = document.getElementById('email').value;
+    var errores = 0;
+    var email_correcto = comprobarEmail(email);
+    if(!email_correcto){
+        mostrarError("El correo introducido es inadecuado");
+        //document.getElementById('email').value = "";
+        errores = errores + 1;
+    }
 
-    var content = document.getElementById('coment').innerText;
-    console.log(content);
+    var nombre = document.getElementById('nombre').value;
+    var nombre_correcto = comprobarNombre(nombre);
+
+    if(!nombre_correcto){
+        mostrarError("El nombre está vacío");
+        //document.getElementById('email').value = "";
+        errores = errores + 1;
+    }
+    
+
+    var content = document.getElementById('coment').value;
+    var content_correcto = comprobarContenido(content);
+
+    if(!content_correcto){
+        mostrarError("El correo introducido es inadecuado");
+        //document.getElementById('email').value = "";
+        errores = errores + 1;
+    }
+
+    if (errores == 0){
+        const d = new Date();
+        var f = d.getDate() + "/" + d.getMonth()+ "/" + d.getFullYear();
+        var h = d.getHours() + ":" + d.getMinutes();
+        var com = {
+            autor: nombre,
+            fecha: f,
+            hora: h,
+            texto: content
+        };
+        comentarios.push(com);
+        vaciarComentarios();
+        mostrarComentarios();
+    }
+    
 
 }
