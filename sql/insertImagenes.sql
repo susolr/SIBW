0cf3b7620e55579a51c7b35c020e4941f44fb81c
susolr <jlopezpeque@gmail.com>
2022-04-18 03:29:36 +0200

    Problemas inyeccion sql

--- sql/insertImagenes.sql
+++ sql/insertImagenes.sql
@@ -1,5 +1,6 @@
 INSERT INTO `imagenes`(`producto`, `recurso`) VALUES ( 1 ,'images/saxo_soprano.png'); 
 INSERT INTO `imagenes`(`producto`, `recurso`) VALUES ( 2 ,'images/saxo_alto.png');
+INSERT INTO `imagenes`(`producto`, `recurso`) VALUES ( 2 ,'images/saxo_alto_custom.png');
 INSERT INTO `imagenes`(`producto`, `recurso`) VALUES ( 3 ,'images/saxo_tenor.png');
 INSERT INTO `imagenes`(`producto`, `recurso`) VALUES ( 4 ,'images/saxo_baritono.png');
 INSERT INTO `imagenes`(`producto`, `recurso`) VALUES ( 5 ,'images/boquilla_selmer_c1.png');
@@ -7,3 +8,4 @@ INSERT INTO `imagenes`(`producto`, `recurso`) VALUES ( 6 ,'images/boquilla_selme
 INSERT INTO `imagenes`(`producto`, `recurso`) VALUES ( 7 ,'images/boquilla_vandoren_al3.png');
 INSERT INTO `imagenes`(`producto`, `recurso`) VALUES ( 8 ,'images/canias_vandoren.png');
 INSERT INTO `imagenes`(`producto`, `recurso`) VALUES ( 9 ,'images/brazadera_bg.png');
+
